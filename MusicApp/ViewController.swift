6d3e1aa3e1a4b603af3c256fc3abf58cd26b3c66
Terashima <yamanogomora@gmail.com>
2019-04-17 09:46:16 +0900

    Add: UIをコードに紐付けて音を再生する処理を実装

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    var cymbalPlayer = AVAudioPlayer()
 
-
+    @IBAction func cymbal(_ sender: Any) {
+        do {
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            cymbalPlayer.play()
+        } catch {
+            print("シンバルで、エラーが発生しました！")
+        }
+    }
+    
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    var guitarPlayer = AVAudioPlayer()
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            guitarPlayer.play()
+        } catch {
+            print("ギターで、エラーが発生しました！")
+        }
+    }
+    
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    var backmusicPlayer = AVAudioPlayer()
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
+            backmusicPlayer.numberOfLoops = -1
+            backmusicPlayer.play()
+        } catch {
+            print("エラーが発生しました！")
+        }
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
+    
+    
 }
 
